28b2c1b499618ee4145f6fbf94da193c3a05e000
Arya Saputra <aryasaputra1304@gmail.com>
2026-01-05 14:15:54 +0700

    Upload struktur folder tugas besar PP2

---
+++ controller/AnggotaController.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package id.ac.unpas.Tubes.controller;
+
+
+/**
+ *
+ * @author Arya
+ */
+public class AnggotaController {
+     
+}
